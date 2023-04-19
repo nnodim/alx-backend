56185ab4a92da315355daf29814f14b3671838c8
nnodim <nnodimobinna@gmail.com>
2023-04-20 00:56:03 +0100

    0. Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""0. Basic dictionary
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+class BasicCache(BaseCaching):
+    """
+    A class BasicCache that inherits from BaseCaching and is a caching system:
+    """
+
+    def __init__(self):
+        """Initialize the cache data"""
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Store a key-value pair
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Return the value that corresponds to the key in the cached data
+        """
+        if key is not None and key in self.cache_data.key():
+            return self.cache_data[key]
+        return None
