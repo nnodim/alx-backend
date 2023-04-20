afbeb9da1ada6fa6b9d168fe4ebab6cfbdf3a938
nnodim <nnodimobinna@gmail.com>
2023-04-20 01:51:41 +0100

    4. MRU Caching

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -16,4 +16,28 @@ class MRUCache(BaseCaching):
         Initialize the cache data
         """
         super().__init__()
-        self.usage = []
+        self.mru_key = None
+
+    def put(self, key, item):
+        """
+        Store a key-value pair
+        """
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            self.mru_key = key
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                print(f"DISCARD: {self.mru_key}")
+                del self.cache_data[self.mru_key]
+            self.cache_data[key] = item
+            self.mru_key = key
+
+    def get(self, key):
+        """
+        Return the value that corresponds to the key in the cached data
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
