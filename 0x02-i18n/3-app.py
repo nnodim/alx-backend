a56c2e8905f71e17a62323ddff920ff0db9d1014
nnodim <nnodimobinna@gmail.com>
2023-04-26 00:55:34 +0100

    3. Parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+3. Parametrize templates
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config(object):
+    """
+    babel config
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    A get_locale function with the babel.localeselector decorator.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """
+    index route
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
