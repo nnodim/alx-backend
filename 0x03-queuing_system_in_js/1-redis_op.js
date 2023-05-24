0ffecaaa61fafcdab636d3e238baecbfd76bde1b
nnodim <nnodimobinna@gmail.com>
2023-05-24 21:58:22 +0000

    2. Node Redis client and basic operations

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,31 @@
+import redis from 'redis';
+
+// Redis client
+const client = redis.createClient();
+
+// Handle Redis connection
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+// Function to set a new school value in Redis
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+// Function to display the value of a school in Redis
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, res) => {
+    if (err) throw err;
+    console.log(`Value for ${schoolName}: ${res}`);
+  });
+}
+
+// Call the functions
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
